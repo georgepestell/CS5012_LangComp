167c3faf66e5a1e9554912cfcfef69a2ceea546c
George Pestell <ghtpestell@protonmail.com#>
2025-03-11 19:51:15 +0000

    added bigram model

--- p1/p1.py
+++ p1/p1.py
@@ -19,6 +19,9 @@ class HMM:
         self.transition_probs = {}
         self.emission_probs = {}
         self.train(train_sents)
+
+        self.states = list(self.state_counts.keys())
+        self.min_log_prob = -float_info.max
     
     def calculateLambda(self, state):
         uniqueTransitions = len(self.transitions[state])
@@ -106,12 +109,7 @@ class HMM:
             return self.transition_probs[prev_state][curr_state]
         return self.transition_probs[prev_state]["<unk>"]
     
-class Viterbi:
-    def __init__(self, hmm):
-        self.hmm = hmm
-        self.states = list(hmm.state_counts.keys())
-
-    def run(self, sentence):
+    def viterbi(self, sentence):
         T = len(sentence)
         N = len(self.states)
 
@@ -121,7 +119,7 @@ class Viterbi:
 
         # First column
         for i, state in enumerate(self.states):
-            viterbi[i, 0] = math.log(self.hmm.getTransitionProbability("<s>", state)) + math.log(self.hmm.getEmissionProbility(state, sentence[0]))
+            viterbi[i, 0] = math.log(self.getTransitionProbability("<s>", state)) + math.log(self.getEmissionProbility(state, sentence[0]))
         
         # Rest of the columns
         for t in range(1, T):
@@ -130,8 +128,8 @@ class Viterbi:
                 max_index = -1
 
                 for j, prev_state in enumerate(self.states):
-                    transition_prob = max(1e-10, self.hmm.getTransitionProbability(prev_state, state))
-                    emission_prob = max(1e-10, self.hmm.getEmissionProbility(state, sentence[t]))
+                    transition_prob = max(1e-10, self.getTransitionProbability(prev_state, state))
+                    emission_prob = max(1e-10, self.getEmissionProbility(state, sentence[t]))
                     prob = viterbi[j, t-1] + math.log(transition_prob) + math.log(emission_prob)
                     if prob > max_prob:
                         max_prob = prob
@@ -151,11 +149,6 @@ class Viterbi:
 
         return list(reversed(best_path))
 
-class LanguageModel:
-    def __init__(self, hmm): 
-        self.hmm = hmm
-        self.min_log_prob = -float_info.max
-
     ### BEGIN STARTER CODE ###
     # Adding a list of probabilities represented as log probabilities.
     def logsumexp(self, vals):
@@ -172,31 +165,113 @@ class LanguageModel:
         total_log_prob = 0
         total_words = 0
         for sentence in test_sents:
-            words = ["<s>"] + [token['form'] for token in sentence] + ["</s>"]
+
+            # Add an extra end of sentence token to the sentence
+            words = [token['form'] for token in sentence] + ["</s>"]
             T = len(words)
-            N = len(self.hmm.state_counts)
+            N = len(self.state_counts)
             forward = np.zeros((N, T))
-            states = list(self.hmm.state_counts.keys())
+            states = list(self.state_counts.keys())
 
             # Initialize first column (in log space)
             for i, state in enumerate(states):
-                forward[i, 0] = math.log(max(1e-10, self.hmm.getTransitionProbability("<s>", state))) + \
-                        math.log(max(1e-10, self.hmm.getEmissionProbility(state, words[0])))
+                forward[i, 0] = math.log(max(1e-10, self.getTransitionProbability("<s>", state))) + \
+                        math.log(max(1e-10, self.getEmissionProbility(state, words[0])))
 
             # Fill rest of the table (in log space)
             for t in range(1, T):
                 for i, state in enumerate(states):
                     log_probs = []
                     for j, prev_state in enumerate(states):
-                        trans_prob = max(1e-10, self.hmm.getTransitionProbability(prev_state, state))
+                        trans_prob = max(1e-10, self.getTransitionProbability(prev_state, state))
                         log_probs.append(forward[j, t-1] + math.log(trans_prob))
                         
                     forward[i, t] = self.logsumexp(log_probs) + \
-                        math.log(max(1e-10, self.hmm.getEmissionProbility(state, words[t])))
+                        math.log(max(1e-10, self.getEmissionProbility(state, words[t])))
 
             # Sum final column for sentence probability (already in log space)
             sentence_log_prob = self.logsumexp(forward[:, T-1])
             total_log_prob += sentence_log_prob / math.log(2)  # convert to log base 2
+            total_words += len(sentence)
+
+        perplexity = math.pow(2, -total_log_prob / total_words)
+        return perplexity
+
+class BigramModel:
+    def __init__(self, train_sents):
+        self.bigrams = {}
+        self.unigrams = {}
+        self.total_tokens = 0
+        self.bigram_probs = {}
+        self.calculateBigrams(train_sents)
+    
+    def calculateBigrams(self, train_sents):
+        self.bigrams = {}
+        self.unigrams = {}
+        total_tokens = 0
+
+        # Count unigrams and bigrams
+        for sentence in train_sents:
+            words = ["<s>"]
+            for token in sentence:
+                words.append(token['form'])
+            words.append("</s>")
+            
+            # Count unigrams
+            for word in words:
+                if word not in self.unigrams:
+                    self.unigrams[word] = 0
+                self.unigrams[word] += 1
+                total_tokens += 1
+            
+            # Count bigrams
+            for i in range(len(words)-1):
+                if words[i] not in self.bigrams:
+                    self.bigrams[words[i]] = {}
+                if words[i+1] not in self.bigrams[words[i]]:
+                    self.bigrams[words[i]][words[i+1]] = 0
+                self.bigrams[words[i]][words[i+1]] += 1
+                # Convert counts to probabilities with Witten-Bell smoothing
+                self.bigram_probs = {}
+                for w1 in self.bigrams:
+                    unique_following = len(self.bigrams[w1])  # Number of unique words following w1
+                    lambda_wb = self.unigrams[w1] / (self.unigrams[w1] + unique_following)
+                    
+                    self.bigram_probs[w1] = {}
+                    for w2 in self.bigrams[w1]:
+                    # Smoothed probability combines ML estimate with unigram probability
+                        bigram_ml = self.bigrams[w1][w2] / self.unigrams[w1]
+                        unigram_prob = self.unigrams[w2] / total_tokens
+                        self.bigram_probs[w1][w2] = lambda_wb * bigram_ml + (1 - lambda_wb) * unigram_prob
+                    
+                    # Add probability for unseen words
+                    unseen_prob = (1 - lambda_wb) * (1 - sum(self.bigram_probs[w1].values()))
+                    self.bigram_probs[w1]["<unk>"] = unseen_prob
+    
+    def calculatePerplexity(self, test_sents):
+
+        total_log_prob = 0
+        total_words = 0
+
+        for sentence in test_sents:
+            # Add start and end tokens
+            words = [token['form'] for token in sentence] + ["</s>"]
+            sentence_log_prob = 0
+            
+            for i in range(1, len(words)):
+                prev_word = words[i-1]
+                curr_word = words[i] 
+                
+                # Get probability, defaulting to unknown word probability if needed
+                if prev_word in self.bigram_probs:
+                    prob = self.bigram_probs[prev_word].get(curr_word, 
+                            self.bigram_probs[prev_word]["<unk>"])
+                else:
+                    prob = 1.0 / len(self.unigrams)  # Uniform distribution for unknown prev_word
+                    
+                sentence_log_prob += math.log(max(prob, 1e-10), 2)
+            
+            total_log_prob += sentence_log_prob
             total_words += len(sentence)
 
         perplexity = math.pow(2, -total_log_prob / total_words)
@@ -211,8 +286,8 @@ if __name__ == '__main__':
 
         ### END STARTER CODE ###
 
+        print(lang)
         hmm = HMM(train_sents)
-        viterbi = Viterbi(hmm)
 
         # Test accuracy
         correct = 0
@@ -220,17 +295,20 @@ if __name__ == '__main__':
         for sentence in test_sents:
             words = [token['form'] for token in sentence]
             tags = [token['upos'] for token in sentence]
-            predicted_tags = viterbi.run(words)
+            predicted_tags = hmm.viterbi(words)
 
             for i in range(len(tags)):
                 if tags[i] == predicted_tags[i]:
                     correct += 1
                 total += 1
-        
-        print(lang, correct / total)
+        print(correct / total)
 
         # Test perplexity
-        lm = LanguageModel(hmm)
-        perplexity = lm.calculatePerplexity(test_sents)
-
+        perplexity = hmm.calculatePerplexity(test_sents)
         print(perplexity)
+
+        # Bigram prob estimation
+        bigram_model = BigramModel(train_sents)
+
+        perplexity_bigrams = bigram_model.calculatePerplexity(test_sents)
+        print(perplexity_bigrams)
